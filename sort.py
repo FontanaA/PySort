02ef5268fda42b2953dd1f0c5965ea1db830a3ad
FontanaA <54959557+FontanaA@users.noreply.github.com>
2020-08-09 18:27:24 -0700

    Update sort.py

---
+++ sort.py
@@ -0,0 +1,11 @@
+import random
+
+def rand_list(srart, stop, total):
+    rand_list = [random.randint(0, 250) for i in range(20)]
+    print(f"{rand_list} \nis the unsorted list")
+    return(rand_list)
+
+def bubble(x, gen):
+    if gen == True:
+        pass
+
