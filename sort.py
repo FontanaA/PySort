ef8ef99e6b0c1ad46bdd7a165d843a4fdd073e67
FontanaA <54959557+FontanaA@users.noreply.github.com>
2020-08-09 19:13:49 -0700

    fixes and updates
    fixed selection, other bugs, and continued

--- sort.py
+++ sort.py
@@ -1,7 +1,10 @@
 import random
 
-def rand_list(srart, stop, total):
-    rand_list = [random.randint(0, 250) for i in range(20)]
+def rand_list(start, stop, total):
+    if start < 0:
+        print("start cannot be < zero ; start reset to zero '0'")
+        start = 0
+    rand_list = [random.randint(start, stop) for i in range(total)]
     print(f"{rand_list} \nis the unsorted list")
     return(rand_list)
 
@@ -10,7 +13,7 @@ def bubble(x):
     while repeat:
         repeat = False
         for i in range (len(x)-1):
-            if [i] < x[i+1]:
+            if x[i] < x[i+1]:
                 x[i], x[i+1] = x[i+1], x[i]
                 print(x)
                 repeat = True
@@ -20,8 +23,11 @@ def selection(x):
     for i in range(len(x)):
         low_val = i
         for j in range(i+1, len(x)):
-            low_val = j
-            x[i], x[low_val] = x[low_val], x[i]
+            if x[j] < x[low_val]:
+                
+                low_val = j
+                x[i], x[low_val] = x[low_val], x[i]
+                print(x)
     return(x)
 
 #the best:
@@ -29,6 +35,7 @@ def stalin(x):
     repeat = True
     while repeat:
         for i in range(len(x)-1):
+            print(x)
             if is_sorted(x):
                 repeat = False
                 return(x)
@@ -49,5 +56,5 @@ def is_sorted(x):
         else:
             return(False) #:(
     return(True)
-            
+bubble(rand_list(0, 100, 100))
                 
