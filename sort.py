35dbefdd52c89dbf1d186e88bfda5d1a8307d146
FontanaA <54959557+FontanaA@users.noreply.github.com>
2020-08-09 18:51:09 -0700

    Update sort.py

--- sort.py
+++ sort.py
@@ -26,4 +26,28 @@ def selection(x):
 
 #the best:
 def stalin(x):
-    
+    repeat = True
+    while repeat:
+        for i in range(len(x)-1):
+            if is_sorted(x):
+                repeat = False
+                return(x)
+            try:
+                if x[i] < x[i+1]:
+                    x.remove(x[i])#may make error | if does make x[i] variable to pass to remove()
+            except:
+                #this is fine.....!
+                pass
+
+def is_sorted(x):
+    yay = 0
+    for i in range(len(x)-1):
+        if x[i] == x[-1]:
+            return(True)
+        if x[i] >= x[i+1]:
+            yay += 1
+        else:
+            return(False) #:(
+    return(True)
+            
+                
